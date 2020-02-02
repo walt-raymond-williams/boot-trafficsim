e83bc98f15824ac32e2d263475845ed2f405f2ed
walt.raymond.williams <walt.raymond.williams@gmail.com>
2020-02-01 18:45:36 -0600

    updated package declarations for simulator package

--- src/main/java/com/walter/boottrafficsim/simulator/SplashFrame.java
+++ src/main/java/com/walter/boottrafficsim/simulator/SplashFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trafficsimproject;
+package com.walter.boottrafficsim.simulator;
 //import sun.awt.image.ToolkitImage;
 import java.awt.Color;
 import java.awt.Font;
